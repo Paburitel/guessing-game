5e3b63740c402efeb07d35baa21073e57ec214da
Paburitel <nik13nik82@gmail.com>
2017-01-27 22:09:27 +0300

    guessing-game/feature: implemented GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.root = null;
+        this.left = null;
+        this.right = null;
+    }
 
     setRange(min, max) {
-
+        this.root = Math.round((max - min)/2)+ min;
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
+        return this.root;
 
     }
 
     lower() {
-
+        this.setRange(this.left, this.root);
     }
 
     greater() {
-
+        this.setRange(this.root, this.right);
     }
 }
 
